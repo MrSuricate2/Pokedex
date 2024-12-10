59b7b3354e6670f4a87f1674a541c83a24c8deb3
MrSuricate <kevindu032270@gmail.com>
2024-12-10 10:19:42 +0100

    refacto

--- app/src/main/java/fr/mrsuricate/pokedex/data/cache/CacheManager.kt
+++ app/src/main/java/fr/mrsuricate/pokedex/data/cache/CacheManager.kt
@@ -3,14 +3,22 @@ package fr.mrsuricate.pokedex.data.cache
 class CacheManager(private val fileManager: FileManager) {
 
     fun <T> cacheValue(data: T, url: String) {
-        if (url == "https://pokeapi.co/api/v2/pokemon") {
-            fileManager.saveData(data.toString(), "pokemon.txt")
-        } else if (url.startsWith("https://pokeapi.co/api/v2/pokemon/")) {
-            val pokemon = url.replace("https://pokeapi.co/api/v2/pokemon/", "")
-            fileManager.saveData(data.toString(), "pokemon_$pokemon.txt")
-        } else if (url == "https://pokeapi.co/api/v2/language") {
-            fileManager.saveData(data.toString(), "language.txt")
+        var fileName = ""
+        when {
+            url == "https://pokeapi.co/api/v2/pokemon" -> {
+                fileName = "pokemon.txt"
+            }
+
+            url.startsWith("https://pokeapi.co/api/v2/pokemon/") -> {
+                val pokemon = url.replace("https://pokeapi.co/api/v2/pokemon/", "")
+                fileName = "pokemon_$pokemon.txt"
+            }
+
+            url == "https://pokeapi.co/api/v2/language" -> {
+                fileName = "language.txt"
+            }
         }
+        fileManager.saveData(data.toString(), fileName)
     }
 
 }
