6ecb45e4ac2651c5d0e66f0744d6f4ad8aaec1fb
MrSuricate <kevindu032270@gmail.com>
2024-12-11 14:40:09 +0100

    refacto CacheManager

--- app/src/main/java/fr/mrsuricate/pokedex/data/cache/CacheManager.kt
+++ app/src/main/java/fr/mrsuricate/pokedex/data/cache/CacheManager.kt
@@ -4,46 +4,29 @@ import com.google.gson.Gson
 
 class CacheManager(private val fileManager: FileManager) {
 
-    fun <T> cacheValue(data: T, url: String) {
-        var fileName = ""
-        val gson = Gson()
-        val jsonData = gson.toJson(data)
-        when {
-            url == "https://pokeapi.co/api/v2/pokemon" -> {
-                fileName = "pokemon.txt"
-            }
-
+    private fun getFileNameFromUrl(url: String): String {
+        return when {
+            url == "https://pokeapi.co/api/v2/pokemon" -> "pokemon.txt"
             url.startsWith("https://pokeapi.co/api/v2/pokemon/") -> {
-                val pokemon = url.replace("https://pokeapi.co/api/v2/pokemon/", "")
-                fileName = "pokemon_$pokemon.txt"
+                val pokemon = url.removePrefix("https://pokeapi.co/api/v2/pokemon/")
+                "pokemon_$pokemon.txt"
             }
 
-            url == "https://pokeapi.co/api/v2/language" -> {
-                fileName = "language.txt"
-            }
+            url == "https://pokeapi.co/api/v2/language" -> "language.txt"
+            else -> throw IllegalArgumentException("URL invalid: $url")
         }
+    }
+
+    fun <T> cacheValue(data: T, url: String) {
+        val fileName = getFileNameFromUrl(url)
+        val jsonData = Gson().toJson(data)
         fileManager.saveData(jsonData, fileName)
     }
 
     fun <T> retrieveValue(url: String, data: Class<T>): T {
-        var fileName = ""
-        when {
-            url == "https://pokeapi.co/api/v2/pokemon" -> {
-                fileName = "pokemon.txt"
-            }
-
-            url.startsWith("https://pokeapi.co/api/v2/pokemon/") -> {
-                val pokemon = url.replace("https://pokeapi.co/api/v2/pokemon/", "")
-                fileName = "pokemon_$pokemon.txt"
-            }
-
-            url == "https://pokeapi.co/api/v2/language" -> {
-                fileName = "language.txt"
-            }
-        }
-
-        val gson = Gson()
-        val result = gson.fromJson(fileManager.loadData(fileName), data)
-        return result
+        val fileName = getFileNameFromUrl(url)
+        val jsonData = fileManager.loadData(fileName)
+        return Gson().fromJson(jsonData, data)
     }
+
 }
